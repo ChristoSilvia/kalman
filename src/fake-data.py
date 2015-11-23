898e697e2483306c196919162dbfa03180b7ef20
Daryl Sew <darylsew@gmail.com>
2015-11-23 14:00:39 -0500

    trying to fix the fake data

--- src/fake-data.py
+++ src/fake-data.py
@@ -21,47 +21,82 @@ class FakeData():
     self.observation_noise = observation_noise
 
     self.last_recorded_time = rospy.get_time()
-    self.position = np.array([0.3, 0.3])
-    self.velocity = np.array([0.01, 0.0042])
+    self.position = np.array([0.2, 0.23])
+    rospy.loginfo(self.position)
+    self.velocity = np.array([0.01,-0.042])
 
     while not rospy.is_shutdown():
-      rospy.loginfo("New data!")
+#      rospy.loginfo("New data!")
       t = rospy.get_time()
-      rospy.loginfo("New time: {0}".format(t))
+#      rospy.loginfo("New time: {0}".format(t))
+      rospy.loginfo(self.position)
       self.advance_time(t - self.last_recorded_time)
       self.last_recorded_time = t
-     
-      sampled_observation_noise = np.random.multivariate_normal(np.zeros(4), self.observation_noise)
-      self.publisher.publish(Vector3(self.position[0], self.position[1], float('nan')))
-      rospy.loginfo("Published Data!")
+#      rospy.loginfo("Finished Position Update")
+#     
+#      sampled_observation_noise = np.random.multivariate_normal(np.zeros(4), self.observation_noise)
+#      self.publisher.publish(Vector3(self.position[0], self.position[1], float('nan')))
+#      rospy.loginfo("Published Data!")
       rate.sleep()
 
   def advance_time(self, t):
     t_left = t
-    while t_left > 0:
-      time, which_wall = self.get_next_event()
-      
-      if time >= t_left:
-        self.position += self.velocity * t_left
-        t_left = 0.0
+#    while t_left > 0:
+#      rospy.loginfo("T_left: {0}".format(t_left))
+#      rospy.loginfo(self.position)
+#      time, which_wall = self.get_next_event()
+#      rospy.loginfo("Next Collision at: {0}".format(time))     
+#      rospy.loginfo("Multiple Bounces: {0}".format(time > t_left))
+# 
+#      if time >= t_left:
+#        self.position += self.velocity * t_left
+#        t_left = -1
+#      else:
+#        self.position += self.velocity * time
+#        if (which_wall is 0) or (which_wall is 1):
+#          self.velocity[0] *= -1
+#        else:
+#          self.velocity[1] *= -1
+#        t_left -= time  
+    rospy.loginfo("T_left: {0}".format(t_left))
+    rospy.loginfo(self.position)
+    time, which_wall = self.get_next_event()
+    rospy.loginfo("Next Collision at: {0}".format(time))     
+    rospy.loginfo("Multiple Bounces: {0}".format(time > t_left))
+ 
+    if time >= t_left:
+      self.position += self.velocity * t_left
+      t_left = -1
+    else:
+      self.position += self.velocity * time
+      if (which_wall is 0) or (which_wall is 1):
+        self.velocity[0] *= -1
       else:
-        self.position += self.velocity * time
-        if (which_wall is 0) or (which_wall is 1):
-          self.velocity[0] *= -1
-        else:
-          self.velocity[1] *= -1
-        t_left -= time  
+        self.velocity[1] *= -1
+      t_left -= time  
+
 
   def get_next_event(self):
+    rospy.loginfo(self.position)
+    rospy.loginfo(self.velocity[0])
+    rospy.loginfo(- self.position[0] / self.velocity[0] )
     t_left_wall_collision = - self.position[0] / self.velocity[0]
     t_right_wall_collision = self.x_length - self.position[0] / self.velocity[0]
     t_bottom_wall_collision = - self.position[1] / self.velocity[1]
     t_top_wall_collision = self.y_length - self.position[1] / self.velocity[1]
 
+    rospy.loginfo("{0}:{1}:{2}:{3}".format(t_left_wall_collision,
+                                             t_right_wall_collision,
+                                             t_bottom_wall_collision,
+                                             t_top_wall_collision ))
+
+
     min_time, which_wall = minimum_positive([t_left_wall_collision,
                                              t_right_wall_collision,
                                              t_bottom_wall_collision,
                                              t_top_wall_collision ])
+
+    rospy.loginfo("{0}, {0}".format(min_time, which_wall))
     return min_time, which_wall
 
 
